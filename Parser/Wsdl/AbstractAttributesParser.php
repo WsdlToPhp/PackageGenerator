6c772e708ec518b5818a9280a8cbd95619524ac1
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-27 16:54:24 +0200

    refactor methods

--- Parser/Wsdl/AbstractAttributesParser.php
+++ Parser/Wsdl/AbstractAttributesParser.php
@@ -2,6 +2,8 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\DomHandler\AttributeHandler;
+
 use WsdlToPhp\PackageGenerator\DomHandler\AbstractAttributeHandler;
 use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\AbstractTag;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
@@ -36,33 +38,50 @@ abstract class AbstractAttributesParser extends AbstractTagParser
     {
         $parent = $tag->getSuitableParent();
         if ($parent instanceof AbstractTag && $tag->hasAttributeName() && ($model = $this->getModel($parent)) instanceof Struct && ($modelAttribute = $model->getAttribute($tag->getAttributeName())) instanceof StructAttribute) {
-            foreach ($tag->getAttributes() as $tagAttribute) {
-                switch ($tagAttribute->getName()) {
-                    case AbstractAttributeHandler::ATTRIBUTE_NAME:
-                        /**
-                         * Avoid this attribute to be added as meta
-                         */
-                        break;
-                    case AbstractAttributeHandler::ATTRIBUTE_TYPE:
-                        $type = $tagAttribute->getValue();
-                        if ($type !== null) {
-                            $typeModel          = $this->generator->getStruct($type);
-                            $modelAttributeType = $modelAttribute->getType();
-                            if ($typeModel instanceof Struct && (empty($modelAttributeType) || strtolower($modelAttributeType) === 'unknown')) {
-                                if ($typeModel->getIsRestriction()) {
-                                    $modelAttribute->setType($typeModel->getName());
-                                } elseif (!$typeModel->getIsStruct() && $typeModel->getInheritance()) {
-                                    $modelAttribute->setType($typeModel->getInheritance());
-                                }
-                            }
-                        }
-                        break;
-                    case AbstractAttributeHandler::ATTRIBUTE_ABSTRACT:
-                        $model->setIsAbstract($tagAttribute->getValue(false, true, 'bool'));
-                        break;
-                    default:
-                        $modelAttribute->addMeta($tagAttribute->getName(), $tagAttribute->getValue(true));
-                        break;
+            $this->parseTagAttributes($tag, $model, $modelAttribute);
+        }
+    }
+    /**
+     * @param AbstractTag $tag
+     * @param Struct $struct
+     * @param StructAttribute $attribute
+     */
+    protected function parseTagAttributes(AbstractTag $tag, Struct $struct, StructAttribute $attribute)
+    {
+        foreach ($tag->getAttributes() as $tagAttribute) {
+            switch ($tagAttribute->getName()) {
+                case AbstractAttributeHandler::ATTRIBUTE_NAME:
+                    /**
+                     * Avoid this attribute to be added as meta
+                     */
+                    break;
+                case AbstractAttributeHandler::ATTRIBUTE_TYPE:
+                    $this->parseTagAttributeType($tagAttribute, $attribute);
+                    break;
+                case AbstractAttributeHandler::ATTRIBUTE_ABSTRACT:
+                    $struct->setIsAbstract($tagAttribute->getValue(false, true, 'bool'));
+                    break;
+                default:
+                    $attribute->addMeta($tagAttribute->getName(), $tagAttribute->getValue(true));
+                    break;
+            }
+        }
+    }
+    /**
+     * @param AttributeHandler $tagAttribute
+     * @param StructAttribute $attribute
+     */
+    protected function parseTagAttributeType(AttributeHandler $tagAttribute, StructAttribute $attribute)
+    {
+        $type = $tagAttribute->getValue();
+        if ($type !== null) {
+            $typeModel          = $this->generator->getStruct($type);
+            $modelAttributeType = $attribute->getType();
+            if ($typeModel instanceof Struct && (empty($modelAttributeType) || strtolower($modelAttributeType) === 'unknown')) {
+                if ($typeModel->getIsRestriction()) {
+                    $attribute->setType($typeModel->getName());
+                } elseif (!$typeModel->getIsStruct() && $typeModel->getInheritance()) {
+                    $attribute->setType($typeModel->getInheritance());
                 }
             }
         }
