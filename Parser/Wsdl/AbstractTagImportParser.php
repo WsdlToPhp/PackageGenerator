e680c09c5cc3d92efd4273d0c8a0a3f2740b2475
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-09 21:58:26 +0200

    fix bug: when schema is imported from an imported schema, we must use the imported schema location instead of the wsdl location as the imported schema can be located elsewhere from the wsdl location.

--- Parser/Wsdl/AbstractTagImportParser.php
+++ Parser/Wsdl/AbstractTagImportParser.php
@@ -12,20 +12,33 @@ abstract class AbstractTagImportParser extends AbstractTagParser
     /**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
-    protected function parseWsdl(Wsdl $wsdl)
+    protected function parseWsdl(Wsdl $wsdl, Schema $schema = null)
     {
         foreach ($this->getTags() as $tag) {
             if ($tag instanceof AbstractTagImport && $tag->getLocationAttribute() != '') {
-                $finalLocation = Utils::resolveCompletePath($wsdl->getName(), $tag->getLocationAttribute());
+                $finalLocation = Utils::resolveCompletePath($this->getLocation($wsdl, $schema), $tag->getLocationAttribute());
                 $this->generator->addSchemaToWsdl($wsdl, $finalLocation);
             }
         }
+    }
+    /**
+     * @param Wsdl $wsdl
+     * @param Schema $schema
+     * @return string
+     */
+    protected function getLocation(Wsdl $wsdl, Schema $schema = null)
+    {
+        $model = $wsdl;
+        if ($schema instanceof Schema) {
+            $model = $schema;
+        }
+        return $model->getName();
     }
     /**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseSchema()
      */
     protected function parseSchema(Wsdl $wsdl, Schema $schema)
     {
-        $this->parseWsdl($wsdl);
+        $this->parseWsdl($wsdl, $schema);
     }
 }
