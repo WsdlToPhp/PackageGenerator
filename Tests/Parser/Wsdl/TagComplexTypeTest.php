b620831fc14963131b938829cd38660c46c098ff
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-09 22:34:05 +0200

    add unit test for code coverage

--- Tests/Parser/Wsdl/TagComplexTypeTest.php
+++ Tests/Parser/Wsdl/TagComplexTypeTest.php
@@ -14,6 +14,13 @@ class TagComplexTypeTest extends WsdlParser
         return new TagComplexType(self::generatorInstance(self::wsdlEbayPath()));
     }
     /**
+     * @return \WsdlToPhp\PackageGenerator\Parser\Wsdl\TagComplexType
+     */
+    public static function partnerInstance()
+    {
+        return new TagComplexType(self::generatorInstance(self::wsdlPartnerPath()));
+    }
+    /**
      *
      */
     public function testParseEbay()
@@ -27,6 +34,24 @@ class TagComplexTypeTest extends WsdlParser
         if ($structs->count() > 0) {
             $this->assertTrue($structs->getStructByName('AbstractRequestType')->getIsAbstract());
             $this->assertTrue($structs->getStructByName('AbstractResponseType')->getIsAbstract());
+            $ok = true;
+        }
+        $this->assertTrue((bool)$ok);
+    }
+    /**
+     *
+     */
+    public function testParseOrderContract()
+    {
+        $tagComplexTypeParser = self::partnerInstance();
+
+        $tagComplexTypeParser->parse();
+
+        $ok = false;
+        $structs = $tagComplexTypeParser->getGenerator()->getStructs();
+        if ($structs->count() > 0) {
+            $this->assertSame('true', $structs->getStructByName('MemberNumber')->getMetaValue('nillable'));
+            $this->assertSame('tns:MemberNumber', $structs->getStructByName('MemberNumber')->getMetaValue('type'));
             $ok = true;
         }
         $this->assertTrue((bool)$ok);
