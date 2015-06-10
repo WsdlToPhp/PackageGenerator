6c7d9e2e0a3f873d8999b8c3802468d60061ca1c
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-11 01:00:05 +0200

    cosmetic: update method comment

--- DomHandler/Wsdl/Tag/TagDocumentation.php
+++ DomHandler/Wsdl/Tag/TagDocumentation.php
@@ -14,11 +14,10 @@ class TagDocumentation extends AbstractTag
         return $this->getNodeValue();
     }
     /**
-     * Finds parent node of this documentation node without taking care of the name attribute for enumeration and definitions
-     * This case is managed first because enumerations are contained by elements and the method could climb to its parent without stopping on the enumeration tag
-     * Indeed, depending on the node, it may contain or not the attribute named "name" so we have to split each case
-     * Go from the deepest possible node to the highest possible node
-     * Each case must be treated on the same level, this is why we test the suitableParent for each case
+     * Finds parent node of this documentation node without taking care of the name attribute for enumeration.
+     * This case is managed first because enumerations are contained by elements and
+     * the method could climb to its parent without stopping on the enumeration tag.
+     * Indeed, depending on the node, it may contain or not the attribute named "name" so we have to split each case.
      * @see \WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\AbstractTag::getSuitableParent()
      */
     public function getSuitableParent($checkName = true, array $additionalTags = array(), $maxDeep = self::MAX_DEEP, $strict = false)
