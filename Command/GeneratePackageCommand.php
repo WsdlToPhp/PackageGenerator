b12e71d7ff1385a38416079e7a90a59765555c28
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-27 01:30:16 +0200

    remove shortcut as they're pretty useless and generate error due to symfony/console component issue

--- Command/GeneratePackageCommand.php
+++ Command/GeneratePackageCommand.php
@@ -53,28 +53,28 @@ class GeneratePackageCommand extends AbstractCommand
         $this
             ->setName('wsdltophp:generate:package')
             ->setDescription('Generate package based on options')
-            ->addOption('wsdl-urlorpath', 'wsdl-url', InputOption::VALUE_REQUIRED, 'Url or path to WSDL')
-            ->addOption('wsdl-destination', 'wsdl-dest', InputOption::VALUE_REQUIRED, 'Path to destination directory, where the package will be generated')
-            ->addOption('wsdl-login', 'wsdl-lg', InputOption::VALUE_OPTIONAL, 'Basic authentication login required to access the WSDL url, can be avoided mot of the time')
-            ->addOption('wsdl-password', 'wsdl-pass', InputOption::VALUE_OPTIONAL, 'Basic authentication password required to access the WSDL url, can be avoided mot of the time')
-            ->addOption('wsdl-proxy-host', 'wsdl-prox-ho', InputOption::VALUE_OPTIONAL, 'Use proxy url')
-            ->addOption('wsdl-proxy-port', 'wsdl-prox-po', InputOption::VALUE_OPTIONAL, 'Use proxy port')
-            ->addOption('wsdl-proxy-login', 'wsdl-prox-lo', InputOption::VALUE_OPTIONAL, 'Use proxy login')
-            ->addOption('wsdl-proxy-password', 'wsdl-prox-pa', InputOption::VALUE_OPTIONAL, 'Use proxy password')
-            ->addOption('wsdl-prefix', 'wsdl-prf', InputOption::VALUE_REQUIRED, 'Prepend generated classes')
-            ->addOption('wsdl-namespace', 'wsdl-ns', InputOption::VALUE_OPTIONAL, 'Package classes\' namespace')
-            ->addOption('wsdl-category', 'wsdl-cat', InputOption::VALUE_OPTIONAL, 'First level directory name generation mode (start, end, cat, none)')
-            ->addOption('wsdl-subcategory', 'wsdl-subcat', InputOption::VALUE_OPTIONAL, 'Second level directory name generation mode (start, end, none), disabled if category=cat')
-            ->addOption('wsdl-gathermethods', 'wsdl-gath', InputOption::VALUE_OPTIONAL, 'Gather methods based on operation name mode (start, end)')
-            ->addOption('wsdl-genwsdlclass', 'wsdl-gwc', InputOption::VALUE_OPTIONAL, 'Enable/Disable main Wsdl class generation, you should always enable this option')
-            ->addOption('wsdl-gentutorial', 'wsdl-gt', InputOption::VALUE_OPTIONAL, 'Enable/Disable tutorial file, you should enable this option only on dev')
-            ->addOption('wsdl-genautoload', 'wsdl-gauto', InputOption::VALUE_OPTIONAL, 'Enable/Disable autoload file generation, this is useless if you use composer or your own autoloader')
-            ->addOption('wsdl-sendarrayparam', 'wsdl-sarpar', InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of an array to send the parameters, can be disabled as it will soon removed')
-            ->addOption('wsdl-genericconstants', 'wsdl-cst', InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of generic constants name (ex : ENUM_VALUE_0, ENUM_VALUE_1, etc) or contextual values (ex : VALUE_STRING, VALUE_YES, VALUES_NO, etc)')
-            ->addOption('wsdl-reponseasobj', 'wsdl-obj', InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of object to encapsulate Web Service response')
-            ->addOption('wsdl-inherits', 'wsdl-inh', InputOption::VALUE_OPTIONAL, 'Astracts struct base name to identify abtract structs, can be avoided as it will be soon removed')
-            ->addOption('wsdl-paramsasarray', 'wsdl-pararray', InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of a \'parameters\' parameter in an array to contain request parameters, disabled in most cases')
-            ->addOption('wsdl-addcomments', 'wsdl-com', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Set comments to be used within each generated file');
+            ->addOption('wsdl-urlorpath', null, InputOption::VALUE_REQUIRED, 'Url or path to WSDL')
+            ->addOption('wsdl-destination', null, InputOption::VALUE_REQUIRED, 'Path to destination directory, where the package will be generated')
+            ->addOption('wsdl-login', null, InputOption::VALUE_OPTIONAL, 'Basic authentication login required to access the WSDL url, can be avoided mot of the time')
+            ->addOption('wsdl-password', null, InputOption::VALUE_OPTIONAL, 'Basic authentication password required to access the WSDL url, can be avoided mot of the time')
+            ->addOption('wsdl-proxy-host', null, InputOption::VALUE_OPTIONAL, 'Use proxy url')
+            ->addOption('wsdl-proxy-port', null, InputOption::VALUE_OPTIONAL, 'Use proxy port')
+            ->addOption('wsdl-proxy-login', null, InputOption::VALUE_OPTIONAL, 'Use proxy login')
+            ->addOption('wsdl-proxy-password', null, InputOption::VALUE_OPTIONAL, 'Use proxy password')
+            ->addOption('wsdl-prefix', null, InputOption::VALUE_REQUIRED, 'Prepend generated classes')
+            ->addOption('wsdl-namespace', null, InputOption::VALUE_OPTIONAL, 'Package classes\' namespace')
+            ->addOption('wsdl-category', null, InputOption::VALUE_OPTIONAL, 'First level directory name generation mode (start, end, cat, none)')
+            ->addOption('wsdl-subcategory', null, InputOption::VALUE_OPTIONAL, 'Second level directory name generation mode (start, end, none), disabled if category=cat')
+            ->addOption('wsdl-gathermethods', null, InputOption::VALUE_OPTIONAL, 'Gather methods based on operation name mode (start, end)')
+            ->addOption('wsdl-genwsdlclass', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable main Wsdl class generation, you should always enable this option')
+            ->addOption('wsdl-gentutorial', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable tutorial file, you should enable this option only on dev')
+            ->addOption('wsdl-genautoload', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable autoload file generation, this is useless if you use composer or your own autoloader')
+            ->addOption('wsdl-sendarrayparam', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of an array to send the parameters, can be disabled as it will soon removed')
+            ->addOption('wsdl-genericconstants', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of generic constants name (ex : ENUM_VALUE_0, ENUM_VALUE_1, etc) or contextual values (ex : VALUE_STRING, VALUE_YES, VALUES_NO, etc)')
+            ->addOption('wsdl-reponseasobj', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of object to encapsulate Web Service response')
+            ->addOption('wsdl-inherits', null, InputOption::VALUE_OPTIONAL, 'Astracts struct base name to identify abtract structs, can be avoided as it will be soon removed')
+            ->addOption('wsdl-paramsasarray', null, InputOption::VALUE_OPTIONAL, 'Enable/Disable usage of a \'parameters\' parameter in an array to contain request parameters, disabled in most cases')
+            ->addOption('wsdl-addcomments', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Set comments to be used within each generated file');
     }
 
     /**
@@ -215,7 +215,7 @@ class GeneratePackageCommand extends AbstractCommand
     private function formatArrayForConsole($array)
     {
         array_walk($array, function (&$value, $index) {
-            $value = sprintf("%s: %s", $index, is_scalar($value) ? $value : implode(', ', $value));
+            $value = sprintf("%s: %s", $index, !is_array($value) ? $value : implode(', ', $value));
         });
         return $array;
     }
