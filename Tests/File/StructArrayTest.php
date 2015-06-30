3f470fc647c66447b00e67a60ee952fac1d4469f
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-30 03:07:52 +0200

    add unit test case

--- Tests/File/StructArrayTest.php
+++ Tests/File/StructArrayTest.php
@@ -23,4 +23,21 @@ class StructArrayTest extends AbstractFile
             $this->assertFalse(true, 'Unable to find ArrayOfNewsRelatedSearch struct for file generation');
         }
     }
+    /**
+     *
+     */
+    public function testWriteBingSearchArrayOfWebSearchOption()
+    {
+        $generator = self::bingGeneratorInstance();
+        $generator->setOptionGenerateWsdlClassFile(true);
+        if ($model = $generator->getStruct('ArrayOfWebSearchOption')) {
+            $struct = new ArrayFile($generator, $model->getName(), $this->getTestDirectory());
+            $struct
+                ->setModel($model)
+                ->write();
+            $this->assertSameFileContent('ValidApiStructArrayOfWebSearchOption', $struct);
+        } else {
+            $this->assertFalse(true, 'Unable to find ArrayOfWebSearchOption struct for file generation');
+        }
+    }
 }
