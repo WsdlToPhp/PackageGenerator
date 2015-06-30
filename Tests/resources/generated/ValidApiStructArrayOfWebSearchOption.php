c553384536982ee556cce07af46f57fe2cdb3db8
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-30 03:06:15 +0200

    add new unit test resource

---
+++ Tests/resources/generated/ValidApiStructArrayOfWebSearchOption.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * File for class ApiStructArrayOfWebSearchOption
+ * @package Api
+ * @subpackage Structs
+ * @release 1.1.0
+ */
+/**
+ * This class stands for ArrayOfWebSearchOption Struct
+ * @package Api
+ * @subpackage Structs
+ * @release 1.1.0
+ */
+class ApiStructArrayOfWebSearchOption extends ApiWsdlClass
+{
+    /**
+     * The WebSearchOption
+     * Meta informations extracted from the WSDL
+     * - maxOccurs: unbounded
+     * - minOccurs: 0
+     * @var ApiEnumWebSearchOption
+     */
+    public $WebSearchOption;
+    /**
+     * Constructor method for ArrayOfWebSearchOption
+     * @see parent::__construct()
+     * @param ApiEnumWebSearchOption $webSearchOption
+     * @return ApiStructArrayOfWebSearchOption
+     */
+    public function __construct($webSearchOption = null)
+    {
+        parent::__construct(array('WebSearchOption'=>$webSearchOption), false);
+    }
+    /**
+     * Get WebSearchOption value
+     * @return ApiEnumWebSearchOption|null
+     */
+    public function getWebSearchOption()
+    {
+        return $this->WebSearchOption;
+    }
+    /**
+     * Set WebSearchOption value
+     * @param ApiEnumWebSearchOption $webSearchOption
+     * @return ApiStructArrayOfWebSearchOption
+     */
+    public function setWebSearchOption($webSearchOption)
+    {
+        $this->WebSearchOption = $webSearchOption;
+        return $this;
+    }
+    /**
+     * Returns the current element
+     * @see ApiWsdlClass::current()
+     * @return ApiEnumWebSearchOption
+     */
+    public function current()
+    {
+        return parent::current();
+    }
+    /**
+     * Returns the indexed element
+     * @see ApiWsdlClass::item()
+     * @param int $index
+     * @return ApiEnumWebSearchOption
+     */
+    public function item($index)
+    {
+        return parent::item($index);
+    }
+    /**
+     * Returns the first element
+     * @see ApiWsdlClass::first()
+     * @return ApiEnumWebSearchOption
+     */
+    public function first()
+    {
+        return parent::first();
+    }
+    /**
+     * Returns the last element
+     * @see ApiWsdlClass::last()
+     * @return ApiEnumWebSearchOption
+     */
+    public function last()
+    {
+        return parent::last();
+    }
+    /**
+     * Returns the element at the offset
+     * @see ApiWsdlClass::offsetGet()
+     * @param int $offset
+     * @return ApiEnumWebSearchOption
+     */
+    public function offsetGet($offset)
+    {
+        return parent::offsetGet($offset);
+    }
+    /**
+     * Add element to array
+     * @see ApiWsdlClass::add()
+     * @uses ApiEnumWebSearchOption::valueIsValid()
+     * @param ApiEnumWebSearchOption $item
+     * @return ApiEnumWebSearchOption
+     */
+    public function add($item)
+    {
+        return ApiEnumWebSearchOption::valueIsValid($item) ? parent::add($item) : false;
+    }
+    /**
+     * Returns the attribute name
+     * @see ApiWsdlClass::getAttributeName()
+     * @return string WebSearchOption
+     */
+    public function getAttributeName()
+    {
+        return 'WebSearchOption';
+    }
+    /**
+     * Method called when an object has been exported with var_export() functions
+     * It allows to return an object instantiated with the values
+     * @see ApiWsdlClass::__set_state()
+     * @uses ApiWsdlClass::__set_state()
+     * @param array $array the exported values
+     * @return ApiStructArrayOfWebSearchOption
+     */
+    public static function __set_state(array $array)
+    {
+        return parent::__set_state($array);
+    }
+    /**
+     * Method returning the class name
+     * @return string __CLASS__
+     */
+    public function __toString()
+    {
+        return __CLASS__;
+    }
+}
