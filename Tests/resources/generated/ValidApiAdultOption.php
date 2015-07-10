cb81887186e7503d8a32ccd8d38bbea179376a7b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-10 17:13:41 +0200

    add composer.json template for standalone mode

---
+++ Tests/resources/generated/ValidApiAdultOption.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This class stands for AdultOption Enum
+ * @package Api
+ * @subpackage Enumerations
+ * @release 1.1.0
+ */
+class ApiEnumAdultOption
+{
+    /**
+     * Constant for value 'Off'
+     * @return string 'Off'
+     */
+    const VALUE_OFF = 'Off';
+    /**
+     * Constant for value 'Moderate'
+     * @return string 'Moderate'
+     */
+    const VALUE_MODERATE = 'Moderate';
+    /**
+     * Constant for value 'Strict'
+     * @return string 'Strict'
+     */
+    const VALUE_STRICT = 'Strict';
+    /**
+     * Return true if value is allowed
+     * @uses ApiEnumAdultOption::VALUE_OFF
+     * @uses ApiEnumAdultOption::VALUE_MODERATE
+     * @uses ApiEnumAdultOption::VALUE_STRICT
+     * @param mixed $value value
+     * @return bool true|false
+     */
+    public static function valueIsValid($value)
+    {
+        return in_array($value, array(ApiEnumAdultOption::VALUE_OFF, ApiEnumAdultOption::VALUE_MODERATE, ApiEnumAdultOption::VALUE_STRICT));
+    }
+    /**
+     * Method returning the class name
+     * @return string __CLASS__
+     */
+    public function __toString()
+    {
+        return __CLASS__;
+    }
+}
