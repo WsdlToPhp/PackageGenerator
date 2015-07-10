b4b92302e1935bf8196d794f07c9f0956715d15a
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-11 00:03:14 +0200

    update unit test

---
+++ Tests/resources/generated/ValidApiLogin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace My\Project\Api\ServiceType;
+
+use \WsdlToPhp\PackageBase\AbstractSoapClientBase;
+
+/**
+ * This class stands for Login ServiceType
+ * @package Api
+ * @subpackage Services
+ * @release 1.1.0
+ */
+class ApiLogin extends AbstractSoapClientBase
+{
+    /**
+     * Method to call the operation originally named Login
+     * @uses AbstractSoapClientBase::getSoapClient()
+     * @uses AbstractSoapClientBase::setResult()
+     * @uses AbstractSoapClientBase::getResult()
+     * @uses AbstractSoapClientBase::saveLastError()
+     * @param $login string
+     * @param $password string
+     * @return string|bool
+     */
+    public function Login($login, $password)
+    {
+        try {
+            $this->setResult(self::getSoapClient()->Login($login, $password));
+            return $this->getResult();
+        } catch(\SoapFault $soapFault) {
+            $this->saveLastError(__METHOD__, $soapFault);
+            return false;
+        }
+    }
+    /**
+     * Returns the result
+     * @see AbstractSoapClientBase::getResult()
+     * @return string
+     */
+    public function getResult()
+    {
+        return parent::getResult();
+    }
+    /**
+     * Method returning the class name
+     * @return string __CLASS__
+     */
+    public function __toString()
+    {
+        return __CLASS__;
+    }
+}
