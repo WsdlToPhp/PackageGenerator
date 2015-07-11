8546653fbf1cdc02ff617030863aaf2b123d3831
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-11 10:56:22 +0200

    add unit test files for tutorial file generation

---
+++ Tests/File/TutorialTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Tests\File;
+
+use WsdlToPhp\PackageGenerator\File\Tutorial as TutorialFile;
+
+class TutorialTest extends AbstractFile
+{
+    public function testBing()
+    {
+        $instance = self::bingGeneratorInstance();
+
+        $tutorial = new TutorialFile($instance, '', $this->getTestDirectory());
+        $tutorial->write();
+
+        $this->assertSameFileContent('ValidBingTutorial', $tutorial);
+    }
+}
---
+++ Tests/resources/generated/ValidBingTutorial.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Api;
+
+/**
+ * This file aims to show you how to use this generated package.
+ * In addition, the goal is to show which methods are available and the fist needed parameter(s)
+ * You have to use an associative array such as:
+ * - the key must be a constant beginning with WSDL_ from AbstractSoapClientbase class each generated ServiceType class extends this class
+ * - the value must be the corresponding key value (each option matches a {@link http://www.php.net/manual/en/soapclient.soapclient.php} option)
+ * $options = array(
+ * AbstractSoapClientBase::WSDL_URL => '/var/www/workspace/WsdlToPhp/src/WsdlToPhp/PackageGenerator/Tests/resources/bingsearch.wsdl',
+ * AbstractSoapClientBase::WSDL_TRACE => true,
+ * AbstractSoapClientBase::WSDL_LOGIN => 'you_secret_login',
+ * AbstractSoapClientBase::WSDL_PASSWORD => 'you_secret_password',
+ * );
+ * etc....
+ * Then instantiate the ServiceType class such as:
+ * - $wsdlObject = new PackageNameWsdlClass($wsdl)
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+/**
+ * Minimal options
+ */
+$options = array(
+    AbstractSoapClientBase::WSDL_URL => '/var/www/workspace/WsdlToPhp/src/WsdlToPhp/PackageGenerator/Tests/resources/bingsearch.wsdl',
+);
+/**
+ * Samples for Search ServiceType
+ */
+$search = new Search();
+/**
+ * Sample call for Search operation/method
+ */
+if ($search->Search(new \Api\StructType\ApiSearchRequest()) !== false) {
+    print_r($search->getResult());
+} else {
+    print_r($search->getLastError());
+}
