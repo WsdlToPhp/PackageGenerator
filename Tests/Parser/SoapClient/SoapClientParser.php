6436379bdf81813f63b7c26d7f0217bc303c3d44
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-25 00:10:57 +0200

    remove useless use statement

--- Tests/Parser/SoapClient/SoapClientParser.php
+++ Tests/Parser/SoapClient/SoapClientParser.php
@@ -2,7 +2,6 @@
 
 namespace WsdlToPhp\PackageGenerator\Tests\Parser\SoapClient;
 
-use WsdlToPhp\PackageGenerator\Generator\Generator;
 use WsdlToPhp\PackageGenerator\Tests\TestCase;
 
 abstract class SoapClientParser extends TestCase
