2407002a83f522561071fc8afebc46a58a2dfd7d
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-28 18:38:56 +0200

    mandevilla - remove unused method

--- DomHandler/Wsdl/Tag/TagAppinfo.php
+++ DomHandler/Wsdl/Tag/TagAppinfo.php
@@ -4,11 +4,4 @@ namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
 class TagAppinfo extends AbstractTag
 {
-    /**
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->getNodeValue();
-    }
 }
