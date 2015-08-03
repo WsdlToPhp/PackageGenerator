9cfb75ecbb56b13b1d918449617a3d71d98cb7cb
Mikael DELSOL <delsol.mikael@gmail.com>
2015-08-03 14:03:28 +0200

    mandevilla-generator_aware - add sanity check

--- File/Tutorial.php
+++ File/Tutorial.php
@@ -244,7 +244,7 @@ class Tutorial extends AbstractFile
     protected function getMethodParameter($parameterType, $parameterName = null)
     {
         $parameter = sprintf('%1$s%2$s', (empty($parameterType) && empty($parameterName)) ? '' : '$', empty($parameterName) ? $parameterType : $parameterName);
-        $model = $this->getGenerator()->getStruct($parameterType);
+        $model = $parameterType !== null ? $this->getGenerator()->getStruct($parameterType) : null;
         if ($model instanceof StructModel && $model->getIsStruct() && !$model->getIsRestriction()) {
             $parameter = sprintf('new %s()', $model->getPackagedName(true));
         }
