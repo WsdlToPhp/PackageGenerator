5e9f9888cd969f0da1b17d1c22f16f57f32576c6
Mikael DELSOL <delsol.mikael@gmail.com>
2015-08-03 06:45:15 +0200

    mandevilla-generator_aware - remove useless method and use statement

--- Parser/ParserInterface.php
+++ Parser/ParserInterface.php
@@ -2,14 +2,8 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser;
 
-use WsdlToPhp\PackageGenerator\Generator\Generator;
-
 interface ParserInterface
 {
-    /**
-     * @param Generator $generator
-     */
-    public function __construct(Generator $generator);
     /**
      * This method is called to launch the data parsing
      * If an exception must be throwned, then it must throwned here
