4040762abaab50edb5c5a21799861fb66b13ab34
Mikael DELSOL <delsol.mikael@gmail.com>
2015-08-03 06:44:23 +0200

    mandevilla-generator_aware - Parser/AbstractParser inherits from AbstractGeneratorAware remove useless code

--- Parser/AbstractParser.php
+++ Parser/AbstractParser.php
@@ -2,22 +2,10 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser;
 
-use WsdlToPhp\PackageGenerator\Generator\Generator;
+use WsdlToPhp\PackageGenerator\Generator\AbstractGeneratorAware;
 
-abstract class AbstractParser implements ParserInterface
+abstract class AbstractParser extends AbstractGeneratorAware implements ParserInterface
 {
-    /**
-     * @var Generator
-     */
-    protected $generator;
-    /**
-     *
-     * @param Generator $generator
-     */
-    public function __construct(Generator $generator)
-    {
-        $this->generator = $generator;
-    }
     /**
      * @return string
      */
