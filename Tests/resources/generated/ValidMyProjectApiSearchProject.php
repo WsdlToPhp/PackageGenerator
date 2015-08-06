3b961bd8a7805a804154249d821bd7a68dbfce90
Mikael DELSOL <delsol.mikael@gmail.com>
2015-08-06 21:18:58 +0200

    mandevilla - #13: add unit test resources

---
+++ Tests/resources/generated/ValidMyProjectApiSearchProject.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace My\Project\ServiceType;
+
+use \WsdlToPhp\PackageBase\AbstractSoapClientBase;
+
+/**
+ * This class stands for Search ServiceType
+ * @package Api
+ * @subpackage Services
+ * @release 1.1.0
+ */
+class ApiSearchProject extends AbstractSoapClientBase
+{
+    /**
+     * Method to call the operation originally named Search
+     * @uses AbstractSoapClientBase::getSoapClient()
+     * @uses AbstractSoapClientBase::setResult()
+     * @uses AbstractSoapClientBase::getResult()
+     * @uses AbstractSoapClientBase::saveLastError()
+     * @param \My\Project\StructType\ApiSearchRequestProject $parameters
+     * @return \My\Project\StructType\ApiSearchResponseProject|bool
+     */
+    public function Search(\My\Project\StructType\ApiSearchRequestProject $parameters)
+    {
+        try {
+            $this->setResult(self::getSoapClient()->Search($parameters));
+            return $this->getResult();
+        } catch (\SoapFault $soapFault) {
+            $this->saveLastError(__METHOD__, $soapFault);
+            return false;
+        }
+    }
+    /**
+     * Returns the result
+     * @see AbstractSoapClientBase::getResult()
+     * @return \My\Project\StructType\ApiSearchResponseProject
+     */
+    public function getResult()
+    {
+        return parent::getResult();
+    }
+    /**
+     * Method returning the class name
+     * @return string __CLASS__
+     */
+    public function __toString()
+    {
+        return __CLASS__;
+    }
+}
