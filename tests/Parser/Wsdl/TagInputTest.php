442fecb4a07cf24a43c1b3bd2fef81d88662b6b6
Mikael DELSOL <delsol.mikael@gmail.com>
2015-09-03 14:12:05 +0200

    issue-31 - #31: add unit test for Input tags parser

--- tests/Parser/Wsdl/TagInputTest.php
+++ tests/Parser/Wsdl/TagInputTest.php
@@ -14,11 +14,25 @@ class TagInputTest extends WsdlParser
         return new TagInput(self::generatorInstance(self::wsdlMyBoardPackPath()));
     }
     /**
+     * @return \WsdlToPhp\PackageGenerator\Parser\Wsdl\TagInput
+     */
+    public static function lnpInstance()
+    {
+        return new TagInput(self::generatorInstance(self::wsdlLnpPath()));
+    }
+    /**
      * @return \SoapClient
      */
     public static function myBoardPackSoapClient()
     {
         return new \SoapClient(self::wsdlMyBoardPackPath());
+    }
+    /**
+     * @return \SoapClient
+     */
+    public static function lnpSoapClient()
+    {
+        return new \SoapClient(self::wsdlLnpPath());
     }
     /**
      *
@@ -43,6 +57,34 @@ class TagInputTest extends WsdlParser
         $this->assertSame(128, $count);
     }
     /**
+     *
+     */
+    public function testParseLnp()
+    {
+        $tagInputParser = self::lnpInstance();
+        $soapClient = self::lnpSoapClient();
+
+        $tagInputParser->parse();
+
+        $count = 0;
+        $soapFunctions = $soapClient->__getFunctions();
+        foreach ($soapFunctions as $soapFunction) {
+            $methodData = self::getMethodDataFromSoapFunction($soapFunction);
+            $method = $tagInputParser->getGenerator()->getServiceMethod($methodData['name']);
+            if (strtolower($methodData['parameter']) === TagInput::UNKNOWN) {
+                if (is_array($method->getParameterType())) {
+                    foreach ($method->getParameterType() as $methodParameterType) {
+                        $this->assertNotSame(TagInput::UNKNOWN, strtolower($methodParameterType));
+                    }
+                } else {
+                    $this->assertNotSame(TagInput::UNKNOWN, strtolower($method->getParameterType()));
+                }
+                $count++;
+            }
+        }
+        $this->assertSame(7, $count);
+    }
+    /**
      * @param string $soapFunction
      * @return string[]
      */
@@ -54,10 +96,12 @@ class TagInputTest extends WsdlParser
             $parameterType = '[a-zA-Z_]*';
         }
         $matches = array();
-        preg_match(sprintf('/[a-zA-Z_]*\s([a-zA-Z_]*)\((%s)\s/i', $parameterType), $soapFunction, $matches);
+        preg_match(sprintf('/[a-zA-Z_]*\s([a-zA-Z_]*)\(.*(%s)\s/i', $parameterType), $soapFunction, $matches);
+        $name = isset($matches[1]) ? $matches[1] : '';
+        $parameter = isset($matches[2]) ? $matches[2] : '';
         return array(
-            'name' => $matches[1],
-            'parameter' => $matches[2],
+            'name' => $name,
+            'parameter' => $parameter,
         );
     }
 }
