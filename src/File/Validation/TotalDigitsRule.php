6dd625a5267bff193dfab1fcd36ad7f820df2737
Mikael DELSOL <delsol.mikael@gmail.com>
2016-03-09 09:55:54 +0100

    issue #49 - remove , from string before validating it

--- src/File/Validation/TotalDigitsRule.php
+++ src/File/Validation/TotalDigitsRule.php
@@ -16,7 +16,7 @@ class TotalDigitsRule extends AbstractRule
         $this
             ->getMethod()
                 ->addChild('// validation for constraint: totalDigits')
-                ->addChild(sprintf('if (is_float($%1$s) && strlen(str_replace(array(\' \', \'.\', \'-\', \'+\'), \'\', $%1$s)) !== %2$d) {', $parameterName, $value))
+                ->addChild(sprintf('if (is_float($%1$s) && strlen(str_replace(array(\' \', \'.\', \',\', \'-\', \'+\'), \'\', $%1$s)) !== %2$d) {', $parameterName, $value))
                     ->addChild($this->getMethod()->getIndentedString(sprintf('throw new \InvalidArgumentException(sprintf(\'Invalid value, the value must at most contain %1$d digits, "%%d" given\', strlen(substr($%2$s, strpos($%2$s, \'.\')))), __LINE__);', $value, $parameterName), 1))
                 ->addChild('}');
         return $this;
