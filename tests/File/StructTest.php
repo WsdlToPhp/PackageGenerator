1e0a9079ccec9c8b89d6f8b32813deb0d50248fd
Mikael DELSOL <delsol.mikael@gmail.com>
2017-04-04 22:16:58 +0200

    issue #84 - add unit test for union

--- tests/File/StructTest.php
+++ tests/File/StructTest.php
@@ -329,4 +329,21 @@ class StructTest extends AbstractFile
             $this->assertFalse(true, 'Unable to find SetExpressCheckoutRequestDetailsType struct for file generation');
         }
     }
+    /**
+     *
+     */
+    public function testWriteWhlHotelReservationType()
+    {
+        $generator = self::whlInstance();
+        $generator->setOptionValidation(true);
+        if (($model = $generator->getStruct('HotelReservationType')) instanceof StructModel) {
+            $struct = new StructFile($generator, $model->getName());
+            $struct
+                ->setModel($model)
+                ->write();
+            $this->assertSameFileContent('ValidHotelReservationType', $struct);
+        } else {
+            $this->assertFalse(true, 'Unable to find HotelReservationType struct for file generation');
+        }
+    }
 }
