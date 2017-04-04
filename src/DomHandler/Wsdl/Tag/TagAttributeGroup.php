75f1e50a111ef5fe62233f37dc7adc50ac85ea43
Mikael DELSOL <delsol.mikael@gmail.com>
2017-04-04 15:31:14 +0200

    issue #82 - add getAttributeRef method to TagAttributeGroup class

--- src/DomHandler/Wsdl/Tag/TagAttributeGroup.php
+++ src/DomHandler/Wsdl/Tag/TagAttributeGroup.php
@@ -4,4 +4,13 @@ namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
 class TagAttributeGroup extends AbstractTag
 {
+    const ATTRIBUTE_REF = 'ref';
+    /**
+     * {@inheritDoc}
+     * @see \WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\AbstractTag::getAttributeRef()
+     */
+    public function getAttributeRef()
+    {
+        return $this->hasAttribute(self::ATTRIBUTE_REF) ? $this->getAttribute(self::ATTRIBUTE_REF)->getValue(false, true, 'string') : '';
+    }
 }
