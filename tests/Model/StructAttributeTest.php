e98611ce1a2c0da2a491f86459940f3071503953
Mikael DELSOL <delsol.mikael@gmail.com>
2017-06-27 22:36:04 +0200

    issue #98 - implement new unit tests

--- tests/Model/StructAttributeTest.php
+++ tests/Model/StructAttributeTest.php
@@ -3,6 +3,7 @@
 namespace WsdlToPhp\PackageGenerator\Tests\Model;
 
 use WsdlToPhp\PackageGenerator\Tests\TestCase;
+use WsdlToPhp\PackageGenerator\Model\Service;
 
 class StructAttributeTest extends TestCase
 {
@@ -11,19 +12,41 @@ class StructAttributeTest extends TestCase
      */
     public function testGetUniqueName()
     {
-        $struct = StructTest::instance('Foo', true);
-        $struct->addAttribute('id', 'int');
-        $struct->addAttribute('name', 'string');
-        $struct->addAttribute('Name', 'string');
-
-        $this->assertEquals('id', $struct->getAttribute('id')->getUniqueName());
-        $this->assertEquals('name', $struct->getAttribute('name')->getUniqueName());
-        $this->assertEquals('Name_1', $struct->getAttribute('Name')->getUniqueName());
+        $struct = StructTest::instance('Foo', true)->addAttribute('id', 'int')->addAttribute('name', 'string')->addAttribute('Name', 'string');
+        $this->assertSame('id', $struct->getAttribute('id')->getUniqueName());
+        $this->assertSame('name', $struct->getAttribute('name')->getUniqueName());
+        $this->assertSame('Name_1', $struct->getAttribute('Name')->getUniqueName());
     }
+    /**
+     *
+     */
     public function testGetReservedMethodsInstance()
     {
-        $struct = StructTest::instance('Foo', true);
-        $struct->addAttribute('id', 'int');
+        $struct = StructTest::instance('Foo', true)->addAttribute('id', 'int');
         $this->assertInstanceOf('\WsdlToPhp\PackageGenerator\ConfigurationReader\StructReservedMethod', $struct->getAttribute('id')->getReservedMethodsInstance());
+    }
+    /**
+     *
+     */
+    public function testGetUniqueNameWithConflict()
+    {
+        /**
+         * previous context
+         */
+        $service = new Service(self::getBingGeneratorInstance(), 'Query');
+        $service->addMethod('query', '', '');
+        $service->getMethod('query')->getMethodName();
+        /**
+         * current context
+         */
+        $struct = StructTest::instance('query', true);
+        $struct->addAttribute('query', 'string');
+        $structAttribute = $struct->getAttribute('query');
+        $this->assertSame(0, $structAttribute->getUniqueName());
+        $this->assertSame(0, $structAttribute->getUniqueName());
+        $this->assertSame('query', $structAttribute->getUniqueName('foo'));
+        $this->assertSame('query', $structAttribute->getUniqueName('foo'));
+        $this->assertSame('getQuery', $structAttribute->getGetterName());
+        $this->assertSame('setQuery', $structAttribute->getSetterName());
     }
 }
