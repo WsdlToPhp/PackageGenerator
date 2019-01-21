7cae60841680ab572263ea2f6306ec89294d49ff
Mikaël DELSOL <contact@mikael-delsol.fr>
2019-01-21 13:25:10 +0100

    issue #158 - add list property the list property is set when it is found as being part of a list tag by the TagList parser

---
+++ src/File/Validation/AbstractSetOfValuesRule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikael
+ * Date: 21/01/19
+ * Time: 12:32
+ */
+
+namespace WsdlToPhp\PackageGenerator\File\Validation;
+
+
+abstract class AbstractSetOfValuesRule
+{
+
+}
---
+++ src/File/Validation/ListRule.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\File\Validation;
+
+use WsdlToPhp\PackageGenerator\File\StructEnum;
+use WsdlToPhp\PackageGenerator\Model\Struct;
+
+class ArrayRule extends AbstractRule
+{
+    /**
+     * @see \WsdlToPhp\PackageGenerator\File\Validation\AbstractValidation::addRule()
+     * @param string $parameterName
+     * @param mixed $value
+     * @param bool $itemType
+     * @return ArrayRule
+     */
+    public function applyRule($parameterName, $value, $itemType = false)
+    {
+        if ($this->getAttribute()->isArray()) {
+            $model = $this->getFile()->getRestrictionFromStructAttribute($this->getAttribute());
+            $itemName = sprintf('%s%sItem', lcfirst($this->getFile()->getModel()->getCleanName(false)), ucfirst($this->getAttribute()->getCleanName()));
+            $this->getMethod()->addChild('// validation for constraint: array');
+            if ($model instanceof Struct) {
+                $this->getMethod()
+                    ->addChild('$invalidValues = array();')
+                    ->addChild(sprintf('foreach ($%s as $%s) {', $parameterName, $itemName))
+                    ->addChild($this->getMethod()->getIndentedString(sprintf('if (!%s::%s($%s)) {', $model->getPackagedName(true), StructEnum::METHOD_VALUE_IS_VALID, $itemName), 1))
+                    ->addChild($this->getMethod()->getIndentedString(sprintf('$invalidValues[] = var_export($%s, true);', $itemName), 2))
+                    ->addChild($this->getMethod()->getIndentedString('}', 1))
+                    ->addChild('}')
+                    ->addChild('if (!empty($invalidValues)) {')
+                    ->addChild($this->getMethod()->getIndentedString(sprintf('throw new \InvalidArgumentException(sprintf(\'Value(s) "%%s" is/are invalid, please use one of: %%s\', implode(\', \', $invalidValues), implode(\', \', %s::%s())), __LINE__);', $model->getPackagedName(true), StructEnum::METHOD_GET_VALID_VALUES), 1))
+                    ->addChild('}');
+            } else {
+                $this->getMethod()->addChild(sprintf('foreach ($%s as $%s) {', $parameterName, $itemName));
+                $this->getRules()->getItemTypeRule()->applyRule($itemName, true);
+                $this->getMethod()->addChild('}');
+            }
+        }
+        return $this;
+    }
+}
