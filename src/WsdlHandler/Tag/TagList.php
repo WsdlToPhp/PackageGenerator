7da9ebf06c291a9b6081ee36e55d20458d80315a
Mikaël DELSOL <contact@mikael-delsol.fr>
2019-01-21 13:43:11 +0100

    issue #158 - add helper method getItemType

--- src/WsdlHandler/Tag/TagList.php
+++ src/WsdlHandler/Tag/TagList.php
@@ -2,14 +2,33 @@
 
 namespace WsdlToPhp\PackageGenerator\WsdlHandler\Tag;
 
+use WsdlToPhp\PackageGenerator\WsdlHandler\AbstractDocument;
+
 class TagList extends Tag
 {
     const ATTRIBUTE_ITEM_TYPE = 'itemType';
+
     /**
      * @return string
      */
     public function getAttributeItemType()
     {
         return $this->hasAttribute(self::ATTRIBUTE_ITEM_TYPE) ? $this->getAttribute(self::ATTRIBUTE_ITEM_TYPE)->getValue() : '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getItemType()
+    {
+        $itemType = $this->getAttributeItemType();
+        // this means this is its simpleType's restriction child element that defines its type
+        if (empty($itemType)) {
+            $child = $this->getChildByNameAndAttributes(AbstractDocument::TAG_RESTRICTION, []);
+            if ($child instanceof TagRestriction && $child->hasAttributeBase()) {
+                $itemType = $child->getAttributeBase();
+            }
+        }
+        return $itemType;
     }
 }
