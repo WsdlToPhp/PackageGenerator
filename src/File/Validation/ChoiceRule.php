4170b65691350b6c06bd99623c2fe041a5573224
Mikaël DELSOL <contact@mikael-delsol.fr>
2019-01-25 23:42:11 +0100

    encure $test exists

--- src/File/Validation/ChoiceRule.php
+++ src/File/Validation/ChoiceRule.php
@@ -25,6 +25,7 @@ class ChoiceRule extends AbstractRule
      */
     public function testConditions($parameterName, $value, $itemType = false)
     {
+        $test = '';
         if (is_array($value) && 0 < count($value)) {
             $this->addValidationMethod($parameterName, $value);
             $test = sprintf('\'\' !== ($message = self::%s($%s))', $this->getValidationMethodName($parameterName), $parameterName);
