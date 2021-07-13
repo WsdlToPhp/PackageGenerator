928233008b67252131627edaa189863f9885b61a
Mikaël DELSOL <contact@mikael-delsol.fr>
2021-07-13 23:10:02 +0200

    format source

--- tests/File/Validation/MaxOccursRuleTest.php
+++ tests/File/Validation/MaxOccursRuleTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace WsdlToPhp\PackageGenerator\Tests\File\Validation;
 
-use StructType\ApiParagraphType;
 use InvalidArgumentException;
+use StructType\ApiParagraphType;
 
 /**
  * @internal
